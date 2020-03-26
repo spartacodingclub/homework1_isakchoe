21739221a69f5db85fcae0dc7c63bec9182b9623
isakchoe <50509176+isakchoe@users.noreply.github.com>
2020-03-26 19:39:44 +0900

    Update gne_crw.py
    숙제 최종

--- gne_crw.py
+++ gne_crw.py
@@ -10,3 +10,28 @@ data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200
 # 이제 코딩을 통해 필요한 부분을 추출하면 된다.
 soup = BeautifulSoup(data.text, 'html.parser')
 
+songs = soup.select('.list-wrap > tbody > tr > td')
+
+
+rank = 0
+
+for song in songs:
+    a_tag = song.select_one('td.info > a ')
+    a_singer = song.select_one('td.info > a.artist.ellipsis')
+
+
+
+    # body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+
+
+
+    if a_tag is not None and a_singer is not None:
+        rank = rank + 1
+        print(rank, a_tag.text.strip(),a_singer.text)
+
+
+
+
+
+
+
