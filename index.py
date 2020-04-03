1b44dfe1c9a8aa9e60cf20321a1d1bad42d680d1
isakchoe <50509176+isakchoe@users.noreply.github.com>
2020-04-03 13:36:06 +0900

    1차
    디비 생성이 안되서
    주문목록이 리스팅 되지 않습니다.

---
+++ index.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dborder
+
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+
+## API 역할을 하는 부분
+@app.route('/order', methods=['POST'])
+def order_post():
+    name = request.form['name']
+    address = request.form['address']
+    phone = request.form['phone']
+    count = request.form['count']
+
+    order = {
+        'name': name,
+        'address': address,
+        'phone': phone,
+        'count': count
+    }
+
+    db.orders.insert_one(order)
+    return jsonify({'result': 'success', 'msg': '주문이 접수되었습니.'})
+
+
+@app.route('/order', methods=['GET'])
+def order_get():
+    orders = list(db.orders.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'orders': orders})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=500, debug=True)
